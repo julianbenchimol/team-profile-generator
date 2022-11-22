c7ebd89e0fc34aa751f880678b76d201601e3bc0
Julian Benchimol <jbenchimol1999@gmail.com>
2022-11-22 10:42:36 -0500

    fixed teammate creation issue

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Engineer = require('./lib/engineer.js');
 const Intern = require('./lib/intern.js');
 const Manager = require('./lib/manager.js');
 const fs = require('fs');
+const { create } = require('domain');
 
 
 Inquirer
@@ -100,102 +101,159 @@ Inquirer
 
 function createTeammate(answers){
     if(answers.teammateType === 'Manager'){
-        const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-        createHtml(newManager)
-    }
-    else if (answers.teammateType === 'Intern'){
-        const newIntern = new Intern(answers.name, answers.id, answers.email, answers.internSchool);
-        createHtml(newIntern);
+        const newManager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
+        createHtml(newManager);
     }
     else if(answers.teammateType === 'Engineer'){
-        const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.engineerGitHub);
+        const newEngineer = new Engineer(answers.egineerName, answers.engineerId, answers.engineerEmail, answers.engineerGitHub);
         createHtml(newEngineer);
+    }
+    else if(answers.teammateType === 'Intern'){
+        const newIntern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        createHtml(newIntern);
+
     }
 }
 
 function createHtml(employee){
-    const special = null;
-    const employeeCard = null;
+
     if(employee.teammateType === 'Manager'){
-        special = 'office';
+        markup = `<!DOCTYPE html>
+        // <html lang="en">
+        //   <head>
+        //     <!-- Default Head Tags -->
+        //     <meta charset="UTF-8" />
+        //     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        //     <!-- Bootstrap Stylesheet Connection -->
+        //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+        //     <!-- Default Stylesheet Connection -->
+        //     <link rel="stylesheet" href="style.css" />
+        //     <title>Insert Title Here</title>
+        //   </head>
+        
+        //   <body>
+        //     <!-- Body Elements Go Here -->
+        //     <div class = "container">
+        //         <div class = "row" id = "main-row">
+        //         <div class="card" style="width: 18rem;">
+        //         <div class="card-body">
+        //           <h5 class="card-title">${employee.getName()}</h5>
+        //           <p class="card-text">Role: ${employee.getRole()}\n ID: ${employee.getId()}\n Email: ${employee.getEmail()}\n Office Number: ${employee.getOfficeNumber}</p>
+        //         </div>
+        //       </div>   
+        //         </div>
+        //     </div>
+        
+        
+        //     <div>
+        //         <!-- JQuery Connection -->
+        //         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        
+        //         <!-- Bootstrap JS connection -->
+        //         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
+        //         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+        
+        //         <!-- Main Script Connection -->
+        //         <script type = "text/javascript" src = "../index.js"></script>
+        //     </div>
+        
+        //   </body>
+        // </html>`
+       
         return;
     }
     else if (employee.teammateType === 'Intern'){
-        special = 'school';
+        markup = `<!DOCTYPE html>
+        // <html lang="en">
+        //   <head>
+        //     <!-- Default Head Tags -->
+        //     <meta charset="UTF-8" />
+        //     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        //     <!-- Bootstrap Stylesheet Connection -->
+        //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+        //     <!-- Default Stylesheet Connection -->
+        //     <link rel="stylesheet" href="style.css" />
+        //     <title>Insert Title Here</title>
+        //   </head>
+        
+        //   <body>
+        //     <!-- Body Elements Go Here -->
+        //     <div class = "container">
+        //         <div class = "row" id = "main-row">
+        //         <div class="card" style="width: 18rem;">
+        //         <div class="card-body">
+        //           <h5 class="card-title">${employee.getName()}</h5>
+        //           <p class="card-text">Role: ${employee.getRole()}\n ID: ${employee.getId()}\n Email: ${employee.getEmail()}\n School: ${employee.getSchool()}</p>
+        //         </div>
+        //       </div>
+                    
+        //         </div>
+        //     </div>
+        
+        
+        //     <div>
+        //         <!-- JQuery Connection -->
+        //         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        
+        //         <!-- Bootstrap JS connection -->
+        //         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
+        //         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+        
+        //         <!-- Main Script Connection -->
+        //         <script type = "text/javascript" src = "../index.js"></script>
+        //     </div>
+        
+        //   </body>
+        // </html>`
+        
         return;
     }
     else if(employee.teammateType === 'Engineer'){
-        special = 'github';
+        markup = `<!DOCTYPE html>
+        // <html lang="en">
+        //   <head>
+        //     <!-- Default Head Tags -->
+        //     <meta charset="UTF-8" />
+        //     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        //     <!-- Bootstrap Stylesheet Connection -->
+        //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+        //     <!-- Default Stylesheet Connection -->
+        //     <link rel="stylesheet" href="style.css" />
+        //     <title>Insert Title Here</title>
+        //   </head>
+        
+        //   <body>
+        //     <!-- Body Elements Go Here -->
+        //     <div class = "container">
+        //         <div class = "row" id = "main-row">
+        //         <div class="card" style="width: 18rem;">
+        //         <div class="card-body">
+        //           <h5 class="card-title">${employee.getName()}</h5>
+        //           <p class="card-text">Role: ${employee.getRole()}\n ID: ${employee.getId()}\n Email: ${employee.getEmail()}\n Github: <a href = "https://github.com/${employee.getGithub()}"GitHub Profile</a></p>
+        //         </div>
+        //       </div>
+                    
+        //         </div>
+        //     </div>
+        
+        
+        //     <div>
+        //         <!-- JQuery Connection -->
+        //         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        
+        //         <!-- Bootstrap JS connection -->
+        //         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
+        //         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+        
+        //         <!-- Main Script Connection -->
+        //         <script type = "text/javascript" src = "../index.js"></script>
+        //     </div>
+        
+        //   </body>
+        // </html>`
         return;
     }
-    if(special === 'office'){
-        employeeCard = `
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${employee.getName()}</h5>
-          <p class="card-text">Role: ${employee.getRole()}\n ID: ${employee.getId()}\n Email: ${employee.getEmail()}\n Office Number: ${employee.getOfficeNumber}</p>
-        </div>
-      </div>`
-      return;
-    }
-    else if (special === 'school'){
-        employeeCard = `
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${employee.getName()}</h5>
-          <p class="card-text">Role: ${employee.getRole()}\n ID: ${employee.getId()}\n Email: ${employee.getEmail()}\n School: ${employee.getSchool}</p>
-        </div>
-      </div>`
-      return;
-    }
-    else if (special === 'gihub'){
-        const employeeCard = `
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${employee.getName()}</h5>
-          <p class="card-text">Role: ${employee.getRole()}\n ID: ${employee.getId()}\n Email: ${employee.getEmail()}\n Github: <a href = "https://github.com/${employee.getSchool}"GitHub Profile</a></p>
-        </div>
-      </div>`
-      return;
-    }
 
-    const markup = `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <!-- Default Head Tags -->
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <!-- Bootstrap Stylesheet Connection -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
-        <!-- Default Stylesheet Connection -->
-        <link rel="stylesheet" href="style.css" />
-        <title>Insert Title Here</title>
-      </head>
-    
-      <body>
-        <!-- Body Elements Go Here -->
-        <div class = "container">
-            <div class = "row" id = "main-row">
-                ${employeeCard}
-            </div>
-        </div>
-    
-    
-        <div>
-            <!-- JQuery Connection -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    
-            <!-- Bootstrap JS connection -->
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
-    
-            <!-- Main Script Connection -->
-            <script type = "text/javascript" src = "../index.js"></script>
-        </div>
-    
-      </body>
-    </html>`
- 
     fs.writeFile('test.html', markup, (err) =>
     err ? console.error(err) : console.log('Success!'));
 }
